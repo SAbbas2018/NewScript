d6774e98d651602d531202d32ca15075c07a22b5
Syed Abbas <40725153+SAbbas2018@users.noreply.github.com>
2019-07-27 20:49:00 -0400

    Update news.py

--- news.py
+++ news.py
@@ -7,7 +7,7 @@ general_sources = ['google-news', 'abc_news', 'cbc-news', 'google-news-ca']
 categories = ['business', 'entertainment', 'general', 'science', 'sports', 'technology']
 """
 # Example for api request for top headlines in the us
-https://newsapi.org/v2/top-headlines?country=us&apiKey=80e76045083d4f898a4e51b5502346ea
+https://newsapi.org/v2/top-headlines?country=us&apiKey={}
 
 q: Keywords or a phrase to search for.
 """
@@ -23,8 +23,8 @@ def News(user):
         while user_in.lower() not in categories:
             user_in = input('Please pick a valid category from the above choices to find news for: ')
 
-    
-        api_key = '80e76045083d4f898a4e51b5502346ea'
+        #add your api key here
+        api_key = ''
 
         url = 'https://newsapi.org/v2/top-headlines?country={}&category={}&apiKey={}'.format(user._count, user_in, api_key)
     
